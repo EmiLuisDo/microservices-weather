03ac5d9c58856a67ba1c3e29beefa962dc6f711a
emi <emiluisdo@gmail.com>
2022-07-11 16:09:05 -0300

    Add package and initial database configuration

---
+++ CloudWeather.Precipitation/DataAccess/Precipitation.cs
@@ -0,0 +1,11 @@
+
+
+namespace CloudWeather.Precipitation.DataAccess {
+    public class Precipitation{
+        public Guid id {get; set;}
+        public DateTime CreateOn {get; set;}
+        public decimal AmountInches {get; set;}
+        public string WeatherType {get; set;}
+        public string ZipCode {get; set;}
+    }
+}
